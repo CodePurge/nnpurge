69b9547642c5020610b090f214a50eceea4dc2c3
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-06-06 20:36:10 -0700

    enable delete derived data

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "nnpurge",
+    platforms: [
+        .macOS(.v14),
+    ],
     products: [
         .library(
             name: "nnpurge",
