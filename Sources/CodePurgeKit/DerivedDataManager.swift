bc3bd7c89c521f1a0b47bbedf0299e254925f2a7
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-10-26 20:56:58 -0400

    feat(lib): add DerivedDataManager with folder operations

--- Sources/CodePurgeKit/DerivedDataManager.swift
+++ Sources/CodePurgeKit/DerivedDataManager.swift
@@ -5,3 +5,51 @@
 //  Created by Nikolai Nobadi on 10/26/25.
 //
 
+import Files
+
+public struct DerivedDataManager {
+    private let store: PurgeRecordStore
+    private let delegate: DerivedDataDelegate
+    
+    init(store: PurgeRecordStore, delegate: DerivedDataDelegate) {
+        self.store = store
+        self.delegate = delegate
+    }
+}
+
+
+// MARK: - Init
+public extension DerivedDataManager {
+    init() {
+        self.init(store: DefaultPurgeRecordStore(), delegate: DefaultDerivedDataDelegate())
+    }
+}
+
+
+// MARK: - Actions
+public extension DerivedDataManager {
+    func loadFolders() -> [Folder] {
+        return [] // TODO: -
+    }
+    
+    func deleteAllDerivedData() throws {
+        // TODO: - 
+    }
+    
+    func deleteFolders(_ folders: [Folder]) throws {
+        // TODO: -
+    }
+}
+
+
+// MARK: - Dependencies
+protocol PurgeRecordStore {
+    
+}
+
+protocol DerivedDataDelegate {
+    
+}
+
+struct DefaultPurgeRecordStore: PurgeRecordStore { }
+struct DefaultDerivedDataDelegate: DerivedDataDelegate { }
