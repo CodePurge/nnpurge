2ddc222a1b64fa8d995f0a99e81d0fe14f4e24ea
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-10-26 21:48:33 -0400

    refactor(cli): simplify deleteDerivedData control flow with option selection

--- Sources/nnpurge/DerivedDataCommand/DerivedDataController.swift
+++ Sources/nnpurge/DerivedDataCommand/DerivedDataController.swift
@@ -22,25 +22,30 @@ struct DerivedDataController {
 // MARK: - Actions
 extension DerivedDataController {
     func deleteDerivedData(deleteAll: Bool) throws {
+        let option = try selectOption(deleteAll: deleteAll)
         let allFolders = manager.loadFolders()
-        let foldersToDelete: [PurgeFolder]
-
-        if deleteAll {
+        
+        switch option {
+        case .deleteAll:
             try picker.requiredPermission(prompt: "Are you sure you want to delete all derived data?")
-            foldersToDelete = allFolders
-        } else {
-            let option = try picker.requiredSingleSelection("What would you like to do?", items: DeleteOption.allCases)
-
-            switch option {
-            case .deleteAll:
-                try picker.requiredPermission(prompt: "Are you sure you want to delete all derived data?")
-                foldersToDelete = allFolders
-            case .selectFolders:
-                foldersToDelete = picker.multiSelection("Select the folders to delete.", items: allFolders)
-            }
+            
+            try manager.deleteAllDerivedData()
+        case .selectFolders:
+            let foldersToDelete = picker.multiSelection("Select the folders to delete.", items: allFolders)
+            
+            try manager.deleteFolders(foldersToDelete)
         }
-
-        try manager.deleteFolders(foldersToDelete)
     }
 }
 
+
+// MARK: - Private Methods
+private extension DerivedDataController {
+    func selectOption(deleteAll: Bool) throws -> DeleteOption {
+        if deleteAll {
+            return .deleteAll
+        }
+        
+        return try picker.requiredSingleSelection("What would you like to do?", items: DeleteOption.allCases)
+    }
+}
